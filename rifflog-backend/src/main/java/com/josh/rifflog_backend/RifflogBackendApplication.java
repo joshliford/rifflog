f82833fc3dbe3313d3be1902c8365f1145ba818d
joshliford <j_liford1@yahoo.com>
2026-04-20 15:06:05 -0500

    init Spring Boot project with basic configuration and dependencies

---
+++ rifflog-backend/src/main/java/com/josh/rifflog_backend/RifflogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.josh.rifflog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifflogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RifflogBackendApplication.class, args);
+	}
+
+}
---
+++ rifflog-backend/src/test/java/com/josh/rifflog_backend/RifflogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josh.rifflog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifflogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
